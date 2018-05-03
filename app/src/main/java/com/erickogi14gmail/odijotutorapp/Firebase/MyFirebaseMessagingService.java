b5c42ace3916eeb35a58492538c97309e6642e45
erickogi <erickogi14@gmail.com>
2018-05-03 15:51:17 +0300

    V1

--- app/src/main/java/com/erickogi14gmail/odijotutorapp/Firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/erickogi14gmail/odijotutorapp/Firebase/MyFirebaseMessagingService.java
@@ -1,8 +1,19 @@
 package com.erickogi14gmail.odijotutorapp.Firebase;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
+import com.erickogi14gmail.odijotutorapp.R;
 import com.erickogi14gmail.odijotutorapp.Views.MainActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -10,29 +21,33 @@ import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Objects;
+import java.util.Random;
+
 /**
  * Created by Eric on 10/13/2017.
  */
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
-    private static final String TAG = "MyFirebaseMsgService";
+//    private static final String TAG = "MyFirebaseMsgService";
+//
+//    @Override
+//    public void onMessageReceived(RemoteMessage remoteMessage) {
+//        if (remoteMessage.getData().size() > 0) {
+//            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
+//            try {
+//                JSONObject json = new JSONObject(remoteMessage.getData().toString());
+//                sendPushNotification(json);
+//            } catch (Exception e) {
+//                Log.e(TAG, "Exception: " + e.getMessage());
+//            }
+//        }
+//    }
+//
+//    //this method will display the notification
+//    //We are passing the JSONObject that is received from
+//    //firebase cloud messaging
 
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        if (remoteMessage.getData().size() > 0) {
-            Log.e(TAG, "Data Payload: " + remoteMessage.getData().toString());
-            try {
-                JSONObject json = new JSONObject(remoteMessage.getData().toString());
-                sendPushNotification(json);
-            } catch (Exception e) {
-                Log.e(TAG, "Exception: " + e.getMessage());
-            }
-        }
-    }
-
-    //this method will display the notification
-    //We are passing the JSONObject that is received from
-    //firebase cloud messaging
     private void sendPushNotification(JSONObject json) {
         //optionally we can display the json into log
         Log.e(TAG, "Notification JSON " + json.toString());
@@ -65,6 +80,128 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         } catch (Exception e) {
             Log.e(TAG, "Exception: " + e.getMessage());
         }
+    }
+
+
+
+
+
+
+
+    private String TAG = "taggs";
+
+    public MyFirebaseMessagingService() {
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+        // TODO(developer): Handle FCM messages here.
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        //Log.d(TAG, "From: " + remoteMessage.getFrom());
+
+        // Check if message contains a data payload.
+        if (remoteMessage.getData().size() > 0) {
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
+
+            handleNow(remoteMessage.getData().toString());
+
+        } else {
+            Log.d(TAG, "Message data payload: is lessssss");
+
+        }
+
+        // Check if message contains a notification payload.
+        if (remoteMessage.getNotification() != null) {
+            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
+        }
+
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
+    }
+    // [END receive_message]
+
+
+    /**
+     * Handle time allotted to BroadcastReceivers.
+     */
+    private void handleNow(String result) {
+        try {
+            JSONObject json = new JSONObject(result);
+            JSONObject data = json.getJSONObject("data");
+            String title = data.getString("title");
+            String message = data.getString("message");
+            //int code = data.getInt("code")
+            // ;
+            //TODO read more attributes here
+            //sendPushNotification(json);
+            sendNotification(title, message);
+
+//            Intent pushNotification = new Intent(Payments.NOTIFICATION);
+//            pushNotification.putExtra("message", message);
+//            pushNotification.putExtra("title", title);
+//            pushNotification.putExtra("code", code);
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+        } catch (Exception e) {
+            sendNotification("bbbb", "mbh");
+//            Intent pushNotification = new Intent(Payments.NOTIFICATION);
+//            pushNotification.putExtra("message", "");
+//            pushNotification.putExtra("title", "");
+//            pushNotification.putExtra("code", 0);
+//            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+            Log.e(TAG, "Exception: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Create and show a simple notification containing the received FCM message.
+     */
+    private void sendNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setAutoCancel(true)
+                        .setSound(defaultSoundUri)
+        .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+       // Objects.requireNonNull(notificationManager).notify(new Random().nextInt()/* ID of notification */, notificationBuilder.build());
+
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setShowWhen(true)
+                .setColor(Color.RED)
+                .setLocalOnly(true)
+                .build();
+
+        NotificationManagerCompat.from(this)
+                .notify(new Random().nextInt(), notification);
+
     }
 
 }
